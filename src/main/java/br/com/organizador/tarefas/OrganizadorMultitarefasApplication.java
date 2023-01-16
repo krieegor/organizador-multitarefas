d0a6e4f92904f8e51e1707bc87d63cda27ffe6af
jprodrigues <joao.sistemas@outlook.com>
2023-01-16 01:36:03 -0300

    Criando Projeto com conexao de banco de dados configurada

---
+++ src/main/java/br/com/organizador/tarefas/OrganizadorMultitarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.organizador.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorMultitarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorMultitarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/organizador/tarefas/OrganizadorMultitarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.organizador.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorMultitarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
