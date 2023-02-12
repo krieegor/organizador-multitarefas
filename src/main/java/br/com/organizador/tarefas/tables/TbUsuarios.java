66b1933a1add55910e78baafb50bd5eef1d2b727
jprodrigues <joao.sistemas@outlook.com>
2023-02-12 00:38:51 -0300

    Finalizacao TbUsuarios e criacao TbLogin

--- src/main/java/br/com/organizador/tarefas/tables/TbUsuarios.java
+++ src/main/java/br/com/organizador/tarefas/tables/TbUsuarios.java
@@ -7,6 +7,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,25 +33,29 @@ public class TbUsuarios {
     private Long cod_pessoa;
 
     @Column(name = "NOME", nullable=false, length=5000)
+    @NotBlank(message = "O campo nome não pode estar em branco")
     private String nome;
 
-    @Column(name = "email")
-    @Email(message = "Email should be valid")
+    @Column(name = "EMAIL")
+    @Email(message = "Endereço de e-mail informado está invalido")
     private String email;
 
     @Column(name = "TELEFONE_CELULAR")
+    @NotBlank(message = "O campo telefone não pode estar em branco")
     private String telefoneCelular;
 
     @Column(name = "TIPO_PESSOA")
     private String tipoPessoa;
 
     @Column(name = "CPF_CNPJ")
+    @NotBlank(message = "O campo cpfCnpj não pode estar em branco")
     private String cpfCnpj;
 
     @Column(name = "NIVEL_ACESSO")
     private String nivelAcesso;
 
     @Column(name = "DATA_INCLUSAO")
+    @NotBlank(message = "O campo dataInclusao não pode estar em branco")
     public LocalDateTime dataInclusao;
 
     @Column(name = "DATA_ALTERACAO")
